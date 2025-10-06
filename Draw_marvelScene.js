49e78f60e15f116512155580eb77de2b3071b9f6
Mara-Seren <maraseren14@gmail.com>
2025-10-06 14:06:34 +1300

    Fixed the broken code
    Fixed the broken code

--- Draw_marvelScene.js
+++ Draw_marvelScene.js
@@ -1,63 +1,24 @@
-
-
+let bgImage, hulkImage, hulkImage3;
 /* load images here */
 function prepareInteraction() {
   hulkImage = loadImage('/images/HFacee.png');
   hulkImage3 = loadImage('/images/HC.png');
   bgImage = loadImage('/images/background.png');
-  
+
 }
 
 let face;
-let captainAmericaSHield = true;
-let WondaHands = true;
+
 
 function drawInteraction(faces, hands) {
 
-image(bgImage, 0, 0, 1280, 960);
+  image(bgImage, 0, 0, 1280, 960);
 
-// This function draw's a dot on all the keypoints. It can be passed a whole face, or part of one. 
-
-// Captain America Shield
-for (let i = 0; i < hands.length; i++) {
-    let hand = hands[i];
-    //console.log(hand);
-    if (showKeypoints) {
-      drawConnections(hand)
-    }
-
-  captainAmericaSHield(hand)
-  }
-  //Wonda Power
-for (let i = 0; i < hands.length; i++) {
-    let hand = hands[i];
-    //console.log(hand);
-    if (showKeypoints) {
-      drawConnections(hand)
-    }
-  WondaHands(hand)
-}
-// Hulk Face
- for (let i = 0; i < faces.length; i++) {
-    let face = faces[i]; // face holds all the keypoints of the face\
-    console.log(face);
-    if (showKeypoints) {
-      drawPoints(face)
-    }
-    
-  }
-}
-
-
-
-
-
-
-
+  // This function draw's a dot on all the keypoints. It can be passed a whole face, or part of one. 
 //Hulk Face
    for (let i = 0; i < faces.length; i++) {
     let face = faces[i]; // face holds all the keypoints of the face\
-    console.log(face);
+    //console.log(face);
     if (showKeypoints) {
       drawPoints(face)
     }
@@ -101,23 +62,68 @@ for (let i = 0; i < hands.length; i++) {
     let mouthX = face.keypoints[19].x;
     let mouthY = face.keypoints[0].y;
 
-   
+
     imageMode (CENTER)
-  
+
    image(hulkImage3, faceCenterX, faceCenterY, 600, 600);
-  
+
    //image(hulkImage2, mouthX, mouthY, lipsWidth, lipsHeight);
    imageMode (CORNER)
 
-   function drawX(X, Y) {
-  push()
-
-  strokeWeight(15)
-  line(X - 20, Y - 20, X + 20, Y + 20)
-  line(X - 20, Y + 20, X + 20, Y - 20)
-
-  pop()
 }
+  // Captain America Shield AND Wanda hands 
+  for (let i = 0; i < hands.length; i++) {
+    let SheildActive = true;
+    let wandaActive = true;
+    let hand = hands[i];
+    //console.log(hand);
+    if (showKeypoints) {
+      drawConnections(hand)
+    }
+ let whatGesture = detectHandGesture(hand)
+    if (whatGesture == "Thumbs Up") {
+      
+      SheildActive = true;
+       captainAmericaSHield(hand)
+    } else {
+      SheildActive = false;
+    }
+
+    if (whatGesture == "Open Palm") {
+      wandaActive = true;
+      WondaHands(hand)
+    } else {
+      wandaActive = false;
+    }
+
+    
+  
+
+  }
+
+
+  //   //Wonda Power
+  // for (let i = 0; i < hands.length; i++) {
+  //     let hand = hands[i];
+  //     //console.log(hand);
+  //     if (showKeypoints) {
+  //       drawConnections(hand)
+  //     }
+  //   WondaHands(hand)
+  // }
+//   // Hulk Face
+//   for (let i = 0; i < faces.length; i++) {
+//     let face = faces[i]; // face holds all the keypoints of the face\
+//     //console.log(face);
+//     if (showKeypoints) {
+//       drawPoints(face)
+//     }
+
+//   }
+// }
+
+
+
 }
 
 //Cap Shield Function
@@ -125,65 +131,60 @@ function captainAmericaSHield(hand) {
   // Find the index finger tip and thumb tip
   // let finger = hand.index_finger_tip;
   let whatGesture = detectHandGesture(hand)
-    if (whatGesture == "Thumbs Up") {
-      captainAmericaSHield = true;
-    }
-    if (whatGesture == "Open Palm") {
-      captainAmericaSHield = false;
-    }
- if (captainAmericaSHield){
-  let finger = hand.middle_finger_tip; // this finger now contains the x and y infomation! you can access it by using finger.x 
-  let thumb = hand.thumb_tip;
 
-  // Draw circles at finger positions
-  let centerX = (finger.x + thumb.x) / 2;
-  let centerY = (finger.y + thumb.y) / 2;
-  // Calculate the pinch "distance" between finger and thumb
-  let pinch = dist(finger.x, finger.y, thumb.x, thumb.y);
+ // if (SheildActive) {
+    let finger = hand.middle_finger_tip; // this finger now contains the x and y infomation! you can access it by using finger.x 
+    let thumb = hand.thumb_tip;
 
-  // This circle's size is controlled by a "pinch" gesture
-  fill(135,0,0);
-  stroke(0);
-  strokeWeight(0);
-  circle(centerX, centerY, 260);
+    // Draw circles at finger positions
+    let centerX = (finger.x + thumb.x) / 2;
+    let centerY = (finger.y + thumb.y) / 2;
+    // Calculate the pinch "distance" between finger and thumb
+    let pinch = dist(finger.x, finger.y, thumb.x, thumb.y);
 
-   fill(165,165,171);
-  stroke(0);
-  strokeWeight(0);
-  circle(centerX, centerY, 200);
+    // This circle's size is controlled by a "pinch" gesture
+    fill(135, 0, 0);
+    stroke(0);
+    strokeWeight(0);
+    circle(centerX, centerY, 260);
 
-   fill(135,0,0);
-  stroke(0);
-  strokeWeight(0);
-  circle(centerX, centerY, 140);
+    fill(165, 165, 171);
+    stroke(0);
+    strokeWeight(0);
+    circle(centerX, centerY, 200);
 
-   fill(0,45,92);
-  stroke(0);
-  strokeWeight(0);
-  circle(centerX, centerY, 80);
+    fill(135, 0, 0);
+    stroke(0);
+    strokeWeight(0);
+    circle(centerX, centerY, 140);
 
-//star shape
-push();
-translate (centerX, centerY);
-//rotate (frameCount * 0.05);
-fill(165,165,171);
-Capstary(0, 0, 40, 16, 5);
-pop();
+    fill(0, 45, 92);
+    stroke(0);
+    strokeWeight(0);
+    circle(centerX, centerY, 80);
+
+    //star shape
+    push();
+    translate(centerX, centerY);
+    //rotate (frameCount * 0.05);
+    fill(165, 165, 171);
+    Capstary(0, 0, 40, 16, 5);
+    pop();
 
 
-  let indexFingerTipX = hand.index_finger_tip.x;
-  let indexFingerTipY = hand.index_finger_tip.y;
-  //fill(0)
-  //circle(indexFingerTipX, indexFingerTipY, 20);
- }
+    let indexFingerTipX = hand.index_finger_tip.x;
+    let indexFingerTipY = hand.index_finger_tip.y;
+    //fill(0)
+    //circle(indexFingerTipX, indexFingerTipY, 20);
+ // }
 }
 
 //Cap Shield Star Helper
-function Capstary (x, y, radius1, radius2, npoints){
+function Capstary(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
-  beginShape ();
-  for (let a = 0; a < TWO_PI; a += angle){
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
     let sx = x + cos(a) * radius2;
     let sy = y + sin(a) * radius2;
     vertex(sx, sy);
@@ -191,51 +192,51 @@ function Capstary (x, y, radius1, radius2, npoints){
     sy = y + sin(a + halfAngle) * radius1;
     vertex(sx, sy);
   }
-  endShape (CLOSE);
+  endShape(CLOSE);
 }
 
 //wonda power Function
 function WondaHands(hand) {
   let whatGesture = detectHandGesture(hand)
-    if (whatGesture == "Thumbs Up") {
-      WondaHands = false;
-    }
-    if (whatGesture == "Open Palm") {
-      WondaHands = true;
-    }
+  // if (whatGesture == "Thumbs Up") {
+  //   wandaActive = false;
+  // }
+  // if (whatGesture == "Open Palm") {
+  //   wandaActive = true;
+  // }
 
- if (WondaHands){
-  let finger = hand.middle_finger_tip; // this finger now contains the x and y infomation! you can access it by using finger.x 
-  let thumb = hand.thumb_tip;   
+ // if (wandaActive) {
+    let finger = hand.middle_finger_tip; // this finger now contains the x and y infomation! you can access it by using finger.x 
+    let thumb = hand.thumb_tip;
 
-   // Draw circles at finger positions
-  let centerX = (finger.x + thumb.x) / 2;
-  let centerY = (finger.y + thumb.y) / 2;
-  // Calculate the pinch "distance" between finger and thumb
-  let pinch = dist(finger.x, finger.y, thumb.x, thumb.y);
+    // Draw circles at finger positions
+    let centerX = (finger.x + thumb.x) / 2;
+    let centerY = (finger.y + thumb.y) / 2;
+    // Calculate the pinch "distance" between finger and thumb
+    let pinch = dist(finger.x, finger.y, thumb.x, thumb.y);
 
-// This circle's size is controlled by a "pinch" gesture
-  fill(64,0,0);
-  stroke(0);
-  strokeWeight(0);
-  circle(centerX, centerY, 260);
+    // This circle's size is controlled by a "pinch" gesture
+    fill(64, 0, 0);
+    stroke(0);
+    strokeWeight(0);
+    circle(centerX, centerY, 260);
 
-   fill(128,0,0);
-  stroke(0);
-  strokeWeight(0);
-  circle(centerX, centerY, 200);
+    fill(128, 0, 0);
+    stroke(0);
+    strokeWeight(0);
+    circle(centerX, centerY, 200);
 
-   fill(191,0,0);
-  stroke(0);
-  strokeWeight(0);
-  circle(centerX, centerY, 140);
+    fill(191, 0, 0);
+    stroke(0);
+    strokeWeight(0);
+    circle(centerX, centerY, 140);
 
-   fill(255,0,0);
-  stroke(0);
-  strokeWeight(0);
-  circle(centerX, centerY, 80);
+    fill(255, 0, 0);
+    stroke(0);
+    strokeWeight(0);
+    circle(centerX, centerY, 80);
 
-}
+ //}
 
 
 }
@@ -247,9 +248,20 @@ function drawPoints(feature) {
   for (let i = 0; i < feature.keypoints.length; i++) {
     let element = feature.keypoints[i];
     noStroke();
-    fill(10,85,25);
+    fill(10, 85, 25);
     rect(element.x, element.y, 8);
   }
   pop()
 
+}
+
+
+   function drawX(X, Y) {
+  push()
+
+  strokeWeight(15)
+  line(X - 20, Y - 20, X + 20, Y + 20)
+  line(X - 20, Y + 20, X + 20, Y - 20)
+
+  pop()
 }
